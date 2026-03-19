7e6556793bf1afa26618948eee49d68cd506431f
Shivani332 <shivaninamdev332@gmail.com>
2026-03-19 14:16:07 +0530

    add contact form in contact page

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,8 +28,10 @@ export default function RootLayout({ children }) {
       >
         <Header />
         {children}
+        <ToastContainer position="top-right" />
         <Footer />
       </body>
     </html>
   );
 }
+
